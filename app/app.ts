5f6c057023053476975bb8df9bad04c45a9d1ebb
Ali Parandeh <Ali.Parandeh@outlook.com>
2020-12-07 10:35:42 +0000

    Add functionality to check for input value of forms

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,28 @@
+function getInputValue(elementID: string): string | undefined {
+    const inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById(elementID);
+    if (inputElement.value === ''){
+        return undefined;
+    }
+    else {
+        return inputElement.value;
+    }
+}
+
+function postScore(score: number, playerName?: string): void {
+    const scoreElement: HTMLElement | null = document.getElementById('postedScores');
+    scoreElement!.innerText = `${score} - ${playerName}`;
+
+}
+
 function startGame(){
     // starting a new game
-    let messagesElement = document.getElementById('messages');
-    messagesElement!.innerText = "Welcome to Multimath...Starting a new game.";
+    let playerName: string | undefined = getInputValue('playername');
+    logPlayer(playerName);
+    postScore(100, playerName);
+}
+
+function logPlayer(name: string = "Multimath Player"): void {
+    console.log(`New game starting for player ${name}`);
 }
 
 document.getElementById('startGame')!.addEventListener('click', startGame);
